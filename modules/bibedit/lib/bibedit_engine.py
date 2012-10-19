94b9fb09cca6a8a5fba9d59ebce3ce45f683b3b6
Sebastian Witowski <sebastian.witowski@cern.ch>
2012-10-19 13:58:31 +0200

    BibEdit: add import from Crossref
    * Adds input field for DOI on record/edit page
    * Miscutil: Adds module crossrefutils, which gets xml from
      crossref page and transforms it into marcxml
    * BibConvert: Adds crossref2marcxml.xsl to translate xml
      from crossref into marcxml

--- modules/bibedit/lib/bibedit_engine.py
+++ modules/bibedit/lib/bibedit_engine.py
@@ -79,7 +79,8 @@ from invenio.bibrecord import create_record, print_rec, record_add_field, \
     record_strip_controlfields
 from invenio.config import CFG_BIBEDIT_PROTECTED_FIELDS, CFG_CERN_SITE, \
     CFG_SITE_URL, CFG_SITE_RECORD, CFG_BIBEDIT_KB_SUBJECTS, \
-    CFG_BIBEDIT_KB_INSTITUTIONS, CFG_BIBEDIT_AUTOCOMPLETE_INSTITUTIONS_FIELDS
+    CFG_BIBEDIT_KB_INSTITUTIONS, CFG_BIBEDIT_AUTOCOMPLETE_INSTITUTIONS_FIELDS, \
+    CFG_INSPIRE_SITE
 from invenio.search_engine import record_exists, search_pattern
 from invenio.webuser import session_param_get, session_param_set
 from invenio.bibcatalog import bibcatalog_system
@@ -98,6 +99,8 @@ from invenio.refextract_api import FullTextNotAvailable
 from invenio import xmlmarc2textmarc as xmlmarc2textmarc
 from invenio.bibdocfile import BibRecDocs, InvenioWebSubmitFileError, \
                                InvenioBibDocFileError
+
+from invenio.crossrefutils import get_marcxml_for_doi, CrossrefError
 
 import invenio.template
 bibedit_templates = invenio.template.load('bibedit')
@@ -514,17 +517,46 @@ def perform_request_record(req, request_type, recid, uid, data, ln=CFG_SITE_LANG
             template_filename = data['templateFilename']
             template = get_record_template(template_filename)
             if not template:
-                response['resultCode']  = 108
+                response['resultCode'] = 108
             else:
                 record = create_record(template)[0]
                 if not record:
-                    response['resultCode']  = 109
+                    response['resultCode'] = 109
                 else:
                     record_add_field(record, '001',
                                      controlfield_value=str(new_recid))
                     create_cache_file(new_recid, uid, record, True)
-                    response['resultCode'], response['newRecID']  = 7, new_recid
+                    response['resultCode'], response['newRecID'] = 7, new_recid
 
+        elif new_type == 'import':
+            # Import data from external source, using DOI
+            doi = data['doi']
+            if not doi:
+                response['resultCode'] = CFG_BIBEDIT_AJAX_RESULT_CODES_REV['error_no_doi_specified']
+            else:
+                try:
+                    marcxml_template = get_marcxml_for_doi(doi)
+                except CrossrefError, inst:
+                    response['resultCode'] = \
+                        CFG_BIBEDIT_AJAX_RESULT_CODES_REV[inst.code]
+                except:
+                    response['resultCode'] = 0
+                else:
+                    if CFG_INSPIRE_SITE:
+                        title_pattern = '<subfield code="p">(.*?)</subfield>'
+                        title_to_change = re.findall(title_pattern, marcxml_template)
+                        if title_to_change:
+                            inspire_title = get_kbr_values("JOURNALS", title_to_change[0], searchtype='s')
+                            if inspire_title:
+                                marcxml_template = re.sub(title_to_change[0], inspire_title[0], marcxml_template)
+                    record = create_record(marcxml_template)[0]
+                    if not record:
+                        response['resultCode'] = 109
+                    else:
+                        record_add_field(record, '001',
+                                         controlfield_value=str(new_recid))
+                        create_cache_file(new_recid, uid, record, True)
+                        response['resultCode'], response['newRecID'] = 7, new_recid
         elif new_type == 'clone':
             # Clone an existing record (from the users cache).
             existing_cache = cache_exists(recid, uid)
