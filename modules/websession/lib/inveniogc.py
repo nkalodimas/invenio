a1cfbb9d13fdd44fd564c8a3d641f6e9e78bf809
Javier Martin Montull <javier.martin.montull@cern.ch>
2013-01-24 11:28:29 +0100

    Inveniogc: remove BibEdit XML files

--- modules/websession/lib/inveniogc.py
+++ modules/websession/lib/inveniogc.py
@@ -85,6 +85,8 @@ CFG_MAX_ATIME_RM_WEBSUBMIT_CKEDITOR_FILE = 28
 # After how many days to remove obsolete temporary files related to BibEdit
 # cache
 CFG_MAX_ATIME_BIBEDIT_TMP = 3
+# After how many days to remove submitted XML files related to BibEdit
+CFG_MAX_ATIME_BIBEDIT_XML = 3
 
 def gc_exec_command(command):
     """ Exec the command logging in appropriate way its output."""
@@ -208,6 +210,12 @@ def clean_tempfiles():
     gc_exec_command('find %s -name "bibedit*.tmp"'
         ' -atime +%s -exec rm %s -f {} \;' \
             % (CFG_TMPSHAREDDIR + '/bibedit-cache/', CFG_MAX_ATIME_BIBEDIT_TMP,
+               vstr))
+
+    write_message("- deleting old XML files submitted via BibEdit")
+    gc_exec_command('find %s -name "bibedit*.xml"'
+        ' -atime +%s -exec rm %s -f {} \;' \
+            % (CFG_TMPSHAREDDIR + '/bibedit-cache/', CFG_MAX_ATIME_BIBEDIT_XML,
                vstr))
 
     write_message("""CLEANING OF TMP FILES FINISHED""")
