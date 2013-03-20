0cca506c43e8b948a3c3b4e134a8a1969962c8a2
Nikolaos Kalodimas <nikolaos.kalodimas@cern.ch>
2013-03-20 05:32:12 +0100

    BibCatalog: fix for ticket_get_info in bibcatalog_system_rt
    * Fixes the parsing of ticket's attachments returned at
      ticket_get_info function of bibcatalog_system_rt.py
    
    Tested-by: Javier Martin Montull <javier.martin.montull@cern.ch>

--- modules/bibcatalog/lib/bibcatalog_system_rt.py
+++ modules/bibcatalog/lib/bibcatalog_system_rt.py
@@ -23,6 +23,7 @@ This is a subclass of BibCatalogSystem
 """
 
 import os
+import re
 import invenio.webuser
 from invenio.shellutils import run_shell_command, escape_shell_arg
 from invenio.bibcatalog_system import BibCatalogSystem, get_bibcat_from_prefs
@@ -328,10 +329,10 @@ class BibCatalogSystemRT(BibCatalogSystem):
             return 0
 
         attachments = []
+        regex = re.compile("[0-9]*:\s{2}[(]")  # attachment's format: 557408:  (text/plain / 131b)
         for line in command_out.split("\n"):
-            if line.count(": ") > 1: #there is a line Attachments: 40: xxx
-                aline = line.split(": ")
-                attachments.append(aline[1])
+            for match in regex.findall(line):
+                attachments.append(match.split(":")[0])
 
         #query again for each attachment
         for att in attachments:
