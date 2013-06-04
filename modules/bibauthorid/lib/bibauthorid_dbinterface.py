e20a372f2113b10c09164a7493a3dab0a577b0fd
Samuele Carli <samuele.carli@cern.ch>
2013-06-04 16:19:57 +0200

    BAI: sql fix

--- modules/bibauthorid/lib/bibauthorid_dbinterface.py
+++ modules/bibauthorid/lib/bibauthorid_dbinterface.py
@@ -3160,9 +3160,9 @@ def get_modified_papers_before(recs, before):   ### filter_modified_record_ids
     @rtype: list [int,]
     '''
     recs_sqlstr = _get_sqlstr_from_set([rec[2] for rec in recs])
-    modified_recs = run_sql("""select id from bibrec
-                               where id in %s
-                               and modification_date < %s"""
+    modified_recs = run_sql("select id from bibrec "
+                            "where id in %s "
+                            "and modification_date < %s "
                                % (recs_sqlstr, before) )
     modified_recs = [rec[0] for rec in modified_recs]
     modified_recs = [rec for rec in recs if rec[2] in modified_recs]
