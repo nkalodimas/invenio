3432d92ac2ec5c8cfe6f9a907984c810af668cc6
Gerrit Rindermann <Gerrit.Rindermann@cern.ch>
2013-06-24 13:06:03 +0200

    Refextract: 5 digits arxiv numbers detection

--- modules/docextract/lib/refextract_tag.py
+++ modules/docextract/lib/refextract_tag.py
@@ -59,7 +59,9 @@ from invenio.refextract_re import \
     re_quoted, \
     re_isbn, \
     re_arxiv, \
+    re_arxiv_5digits, \
     re_new_arxiv, \
+    re_new_arxiv_5digits, \
     re_pos, \
     re_pos_year_num, \
     re_series_from_numeration_after_volume, \
@@ -366,7 +368,9 @@ def tag_arxiv(line):
             u'%(month)s.%(num)s%(suffix)s' \
             u'</cds.REPORTNUMBER>' % groups
 
+    line = re_arxiv_5digits.sub(tagger, line)
     line = re_arxiv.sub(tagger, line)
+    line = re_new_arxiv_5digits.sub(tagger, line)
     line = re_new_arxiv.sub(tagger, line)
     return line
 
