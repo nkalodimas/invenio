eaf457acf895ff0e002d09a45a4d3b40e845abe3
Thorsten Schwander <thorsten.schwander@gmail.com>
2013-06-27 15:01:17 +0200

    BibRank: consider only one year in citation graph
    * When building the citation graph of a paper, consider referrer
      only once (carefully choosing the most appropriate year).

--- modules/bibrank/lib/bibrank_citation_grapher.py
+++ modules/bibrank/lib/bibrank_citation_grapher.py
@@ -53,11 +53,11 @@ def calculate_citation_history_coordinates(recid):
     initial_result= get_initial_result(calculate_citation_graphe_x_coordinates(recid))
     citlist = calculate_cited_by_list(recid)
     for rec_id, _ in citlist:
-        cit_year = get_field_values(rec_id,'773__y')
+        cit_year = get_field_values(rec_id, '269__c')
+        if not cit_year:
+            cit_year = get_field_values(rec_id, '773__y')
         if not cit_year:
             cit_year = get_field_values(rec_id, '260__c')
-            if not cit_year:
-                cit_year = get_field_values(rec_id, '269__c')
         #some records simlpy do not have these fields
         if cit_year:
             #maybe cit_year[0][0:4] has a typo and cannot
