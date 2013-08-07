6fefcc53378aad07c9f6d6aa50f75da4d73e81bc
Samuele Carli <samuele.carli@cern.ch>
2013-08-07 23:22:28 +0200

    BibAuthorID: hotfix temporarily disable
    * Decreasing cache limit when fetching record data to 100 from
      2500 in order to do faster look-ups when 100 or more records
      need updating.

--- modules/bibauthorid/lib/bibauthorid_config.py
+++ modules/bibauthorid/lib/bibauthorid_config.py
@@ -137,7 +137,7 @@ RABBIT_USE_EXTERNAL_ID_INSPIREID = CFG_INSPIRE_SITE
 #Force rabbit to cache entire marc tables instead of querying db if dealing with more
 #then threshold papers
 RABBIT_USE_CACHED_GET_GROUPED_RECORDS = True
-RABBIT_USE_CACHED_GET_GROUPED_RECORDS_THRESHOLD = 2500
+RABBIT_USE_CACHED_GET_GROUPED_RECORDS_THRESHOLD = 100
 
 
 #Cache the personid table for performing exact name searches?
