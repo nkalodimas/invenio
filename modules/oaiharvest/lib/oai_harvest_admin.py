nkalodimas/invenio

commit bc31482b35cc5e77eed23c50135844817be1704c

oaiharvest: improves arXiv identifier harvest
* When specifying identifiers on the command line to oaiharvest,
  you can now specify the short-hand arXiv identifier instead of
  the full OAI identifier.

  BEWARE: Special INSPIRE use-case only as assumptions that arXiv
          is the only OAI source is being made at this point.

* Removes some INSPIRE specific code to keep track of authorlists.

modules/oaiharvest/lib/oai_harvest_daemon.py
@@ -995,11 +995,7 @@ def call_authorlist_extract(active_file, extracted_file, harvested_identifier_li
                     additional_authors = record_get_field_instances(authorlist_record, '700')
                     record_add_fields(existing_record, '100', first_author)
                     record_add_fields(existing_record, '700', additional_authors)
-                    if CFG_INSPIRE_SITE and 'arXiv' in identifier:
-                        # If INSPIRE, then append the arXiv id to the file of records having authorlists
-                        # extracted
-                        filepath = "/afs/cern.ch/project/inspire/info/hep/misc/has_authorlist.txt"
-                        append_arxiv_id_to_file(filepath, identifier)
+
         updated_xml.append(record_xml_output(existing_record))
     updated_xml.append('</collection>')
     # Write to file
@@ -1375,7 +1371,16 @@ def get_dates(dates):
 def get_identifier_names(identifier):
     if identifier:
         # Let's see if the user had a comma-separated list of OAI ids.
-        return [ident.strip() for ident in identifier.split(',')]
+        stripped_idents = []
+        for ident in identifier.split(","):
+            if "arXiv" in ident:
+                # New style arXiv ID
+                ident = ident.replace("arXiv", "oai:arXiv.org")
+            elif "/" in ident:
+                # Old style arXiv ID?
+                ident = "%s%s" % ("oai:arXiv.org:", ident)
+            stripped_idents.append(ident.strip())
+        return stripped_idents
 
 
 def get_repository_names(repositories):
@@ -1617,20 +1622,6 @@ def task_submit_elaborate_specific_parameter(key, value, opts, args):
         return False
     return True
 
-def append_arxiv_id_to_file(filepath, identifier):
-    """
-    Used by INSPIRE site only, to append an arXiv id to a file used
-    to list all arXiv records having their authorlist extracted.
-
-    Only a temporary function until SPIRES shutoff.
-    """
-    id_arxiv = "arXiv:%s" % (re.findall('[a-zA-Z\\-]+/\\d+|\\d+\\.\\d+', identifier)[0],)
-    try:
-        fd_authorlist = open(filepath, 'a')
-        fd_authorlist.write("%s\n" % (id_arxiv,))
-        fd_authorlist.close()
-    except IOError:
-        pass
 ### okay, here we go:
 if __name__ == '__main__':
     main()

commit 885b81b1636ede6813d941c11cde0303a3cfabb5

oaiharvest: fix for identifier parsing
* Fixes a problem parsing correct OAI IDs introduced in bc31482.

modules/oaiharvest/lib/oai_harvest_admin.py
@@ -1121,7 +1121,7 @@ def perform_request_harvest_record(oai_src_id=None, ln=CFG_SITE_LANG, record_id=
     else:
         enable_reporting = True
 
-    form_text = "<p><small>Enter one OAI ID per line. Ex: oai:arXiv.org:hep-th/9901001</small></p>"
+    form_text = "<p><small>Enter one OAI ID per line. Ex: oai:arXiv.org:hep-th/9901001 or arXiv:1308.1234 or hep-th/9901001</small></p>"
     form_text += oaiharvest_templates.tmpl_admin_w200_textarea(ln=ln, title="", \
                                                               name="record_id", value=record_str)
     form_text += oaiharvest_templates.tmpl_admin_checkboxes(ln=ln, \

modules/oaiharvest/lib/oai_harvest_daemon.py
@@ -1373,12 +1373,15 @@ def get_identifier_names(identifier):
         # Let's see if the user had a comma-separated list of OAI ids.
         stripped_idents = []
         for ident in identifier.split(","):
-            if "arXiv" in ident:
-                # New style arXiv ID
-                ident = ident.replace("arXiv", "oai:arXiv.org")
-            elif "/" in ident:
-                # Old style arXiv ID?
-                ident = "%s%s" % ("oai:arXiv.org:", ident)
+            if not ident.startswith("oai:arXiv.org"):
+                if "oai:arxiv.org" in ident.lower():
+                    ident = ident.replace("oai:arxiv.org", "oai:arXiv.org")
+                elif "arXiv" in ident:
+                    # New style arXiv ID
+                    ident = ident.replace("arXiv", "oai:arXiv.org")
+                elif "/" in ident:
+                    # Old style arXiv ID?
+                    ident = "%s%s" % ("oai:arXiv.org:", ident)
             stripped_idents.append(ident.strip())
         return stripped_idents
 